82aee7196421c39067cac6981a188b4d2c024c6d
Bassam Safadi <samsafadi@gmail.com>
2020-04-27 07:30:45 +0000

    First commit

---
+++ catkin_ws/build/ros_astra_camera/CMakeFiles/astra_camera_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/astra_camera_generate_messages_eus"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetCameraInfo.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetUVCWhiteBalance.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetIRExposure.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetLaser.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetUVCGain.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetUVCExposure.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetDeviceType.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetLDP.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetSerial.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetUVCExposure.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetUVCGain.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/ResetIRExposure.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetIRGain.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetUVCWhiteBalance.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SwitchIRCamera.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetIRGain.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/ResetIRGain.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/SetIRFlood.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/srv/GetIRExposure.l"
+  "/root/catkin_ws/devel/share/roseus/ros/astra_camera/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/astra_camera_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
