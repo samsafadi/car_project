6fa2792136c39a0753e5d4fe97f986a605b6134a
Bassam Safadi <samsafadi@gmail.com>
2020-05-16 02:08:36 +0000

    plane segmentation and cluster in experiments

--- catkin_ws/src/tests/src/experiments.cpp
+++ catkin_ws/src/tests/src/experiments.cpp
@@ -6,6 +6,17 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/crop_box.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 using namespace std;
 
@@ -40,12 +51,82 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     boxFilter.setInputCloud(cloud);
     //cout << cloud->size() << endl;
     boxFilter.filter(*cloud_filtered);
-    //cout << cloud_filtered->size() << endl;
     // VoxelGrid Filtering 
     pcl::VoxelGrid<pcl::PointXYZ> sor;
     sor.setInputCloud(cloud_filtered);
     sor.setLeafSize(0.005, 0.005, 0.005);
     sor.filter(*cloud_filtered);
+    //cout << "cloud_filtered size: " << cloud_filtered->size () << endl;
+
+    // Create segmentation object for planar model and set params
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (100);
+    seg.setDistanceThreshold (0.05);
+    
+    int i = 0, nr_points = (int) cloud_filtered->points.size ();
+    while (cloud_filtered->points.size () > 0.3 * nr_points) 
+    {
+        // Segment the largest planar compontent from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+        cout << inliers->indices.size () << endl;
+        if (inliers->indices.size () == 0)
+        {
+            cout << "Could not estimate a planar model for the given dataset." << endl;
+            break;
+        }
+        
+        // Extract the planar inliers from the input cloud
+        pcl::ExtractIndices<pcl::PointXYZ> extract;
+        extract.setInputCloud (cloud_filtered);
+        extract.setIndices (inliers);
+        extract.setNegative (false);
+
+        // Get the points associated with the planar surface
+        extract.filter(*cloud_plane);
+        cout << "PointCloud representing the planar component: " << cloud_plane->size () << " data points." << endl;
+
+        // Remove the planar inliers, extract the rest
+        extract.setNegative (true);
+        extract.filter (*cloud_filtered);
+        //cout << "cloud_filtered size: " << cloud_filtered->size () << endl;
+        
+        ++i;
+    }
+
+    // Create the KdTree for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (cloud_filtered);
+
+    vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (0.02);
+    ec.setMinClusterSize (100);
+    ec.setMaxClusterSize (10000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud_filtered);
+    ec.extract(cluster_indices);
+
+    int j = 0;
+    for (vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+    {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+            cloud_cluster->points.push_back (cloud_filtered->points[*pit]);
+        
+        cloud_cluster->width = cloud_cluster->points.size ();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+
+        std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+        j++;
+    }
 
 
     // Convert to ROS data type
